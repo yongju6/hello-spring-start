686ed40b1a10c5db0c1aa3c3c842f6ccb5fdd3a9
yongjuj <judyjak2@naver.com>
2023-08-14 16:32:33 +0900

    aop 예시 구현(time trace)

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,32 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+
 @Configuration
+@RequiredArgsConstructor
 public class SpringConfig {
+
+    private final MemberRepository memberRepository;
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }
+
+//    @Bean
+//    public MemberRepository memberRepository(){
+//
+//       return new MemoryMemberRepository();
+//       return new JpaMemberRepository(em);
+//    }
 }
